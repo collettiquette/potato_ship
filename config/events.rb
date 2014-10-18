5b53472d273af2c8b39a38aaaa3205d1e935a9ea
JC Wilcox <84jwilcox@gmail.com>
2014-10-17 21:50:14 -0500

    Set up initial websocket connection to PlayersController

--- config/events.rb
+++ config/events.rb
@@ -11,4 +11,9 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
+
+  subscribe :player_connected, to: Sockets::PlayersController,
+                               with_method: :player_connected
+  subscribe :client_disconnected, to: Sockets::PlayersController,
+                                  with_method: :client_disconnected
 end
