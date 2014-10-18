f14f5f852cffe7bab0356fd3bc8544268b6f8db1
JC Wilcox <84jwilcox@gmail.com>
2014-10-18 11:31:48 -0500

    Players automatically removed from game on disconnect

--- app/controllers/sockets/players_controller.rb
+++ app/controllers/sockets/players_controller.rb
@@ -2,22 +2,29 @@ module Sockets
   class PlayersController < WebsocketRails::BaseController
     def initialize_session
       puts 'Initializing session'
-      redis[:games] = [].to_json
     end
 
     def player_connected
       player = Player.find(message[:player_id])
       puts "Player connected #{player.name}"
-      games = JSON.parse(redis[:games])
-      game = find_game(games)
-      game << Stat.new(player: player, game_id: game.id)
-      games << game
-      redis[:games] = games.to_json
+      game = find_game
+      game[client_id] = Stat.new(player: player, game_id: game.id)
+      p client_id
+      games[game.id] = game
+      store_games
       WebsocketRails[:da_game].trigger(:player_connected, { players: game.player_names } )
     end
 
     def client_disconnected
       puts "Player #{client_id} disconnected"
+      games.each do |game_id, game|
+        if game.remove(client_id)
+          break game
+        end
+      end
+      store_games
+      puts "#{client} removed"
+      WebsocketRails[:da_game].trigger(:player_disconnected, {})
     end
 
     private
@@ -26,15 +33,25 @@ module Sockets
       WebsocketRails::Synchronization.singleton.redis
     end
 
-    def find_game(games)
-      game_id = 0
-      games.each_with_index do |game_hash, i|
-        unless Game.parse(game_hash).full?
-          return Game.parse(games.delete_at(i))
+    def games
+      controller_store[:games] ||= JSON.parse(redis[:games] ||= {}.to_json)
+        .each_with_object({}) do |(game_id, game_hash), result|
+          result[game_id.to_i] = Game.parse(game_hash)
         end
-        game_id += 1
+    end
+
+    def store_games
+      redis[:games] = games.to_json
+      controller_store.delete(:games)
+    end
+
+    def find_game
+      games.each do |game_id, game|
+        unless game.full?
+          return games.delete(game_id)
+        end
       end
-      Game.new(game_id + 1)
+      Game.new((games.keys.max || 0) + 1)
     end
   end
 end
