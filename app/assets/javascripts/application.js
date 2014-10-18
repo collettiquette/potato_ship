d8ef3ec193285b6d05048b10dc665dae8dd9a596
Brandon Cash <brandon@40digits.com>
2014-10-17 22:34:07 -0500

    Keyboard controls; deceleration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 var GameEngine = function () {
 
 	var game;
+	var cursors	;
 	var sprite;
 	var enemies;
 
@@ -39,33 +40,59 @@ var GameEngine = function () {
 	}
 
 	var create = function () {
+		// Set up input
+		cursors = game.input.keyboard.createCursorKeys();
 
+		// Set up physics
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		// Set up the player
 		sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
-		sprite.anchor.set(0.5);
-
 		game.physics.arcade.enable(sprite);
+		sprite.anchor.set(0.5);
+		sprite.body.drag.set(100);
+		sprite.body.maxVelocity.set(200);
 
 		// Set up enemies
 		enemies = game.add.group();
 		enemies.enableBody = true;
-		var one = enemies.create(0, 400, 'one');
-		one.scale.setTo(2, 2);
+
+		// Add a few testable enemies
+		for (var i = 0; i < 10; i++) {
+			addEnemy();
+		}
+
+		// A testing key to add an enemy to the world
+		var key_addEnemy = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+		key_addEnemy.onDown.add(addEnemy, this);
+
+	}
+
+	var addEnemy = function () {
+		var x = Math.random() * 800;
+		var y = Math.random() * 600;
+		var one = enemies.create(x, y, 'one');
 		one.body.immovable = true;
 	}
 
 	var update = function () {
 
-		//  If the sprite is > 8px away from the pointer then let's move to it
-		if (game.physics.arcade.distanceToPointer(sprite, game.input.activePointer) > 8) {
-			//  Make the object seek to the active pointer (mouse or touch).
-			game.physics.arcade.moveToPointer(sprite, 300);
+		// Accelerating the player
+		if (cursors.up.isDown) {
+			game.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration);
+		} else if (cursors.down.isDown) {
+			game.physics.arcade.accelerationFromRotation(sprite.rotation, -200, sprite.body.acceleration);
+		} else {
+			sprite.body.acceleration.set(0);
 		}
-		else {
-			//  Otherwise turn off velocity because we're close enough to the pointer
-			sprite.body.velocity.set(0);
+
+		// Turning the player
+		if (cursors.left.isDown) {
+			sprite.body.angularVelocity = -300;
+		} else if (cursors.right.isDown) {
+			sprite.body.angularVelocity = 300;
+		} else {
+			sprite.body.angularVelocity = 0;
 		}
 
 		game.physics.arcade.collide(sprite, enemies);
@@ -73,8 +100,6 @@ var GameEngine = function () {
 	}
 
 	var render = function () {
-
-		game.debug.inputInfo(32, 32);
 
 	}
 
