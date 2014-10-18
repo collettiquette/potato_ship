f5d9f630b64f911ef35b75857d16bf04784aa5b8
Brandon Cash <brandon@40digits.com>
2014-10-17 20:48:07 -0500

    Set up initial game stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,66 @@
 //= require_tree .
 //= require websocket_rails/main
 
+
+var game = function() {
+
+	var phaser;
+	var sprite;
+
+	function init() {
+		console.log('Potato ship engage');
+		phaser = new Phaser.Game(800, 600, Phaser.AUTO, 'game-screen', {
+			preload: preload,
+			create: create,
+			update: update,
+			render: render
+		});
+	}
+
+	function preload() {
+
+		game.load.image('phaser', 'assets/images/player-ship.png');
+
+	}
+
+	function create() {
+
+		//  To make the sprite move we need to enable Arcade Physics
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
+		sprite.anchor.set(0.5);
+
+		//  And enable the Sprite to have a physics body:
+		game.physics.arcade.enable(sprite);
+
+	}
+
+	function update () {
+
+		//  If the sprite is > 8px away from the pointer then let's move to it
+		if (game.physics.arcade.distanceToPointer(sprite, game.input.activePointer) > 8) {
+			//  Make the object seek to the active pointer (mouse or touch).
+			game.physics.arcade.moveToPointer(sprite, 300);
+		}
+		else {
+			//  Otherwise turn off velocity because we're close enough to the pointer
+			sprite.body.velocity.set(0);
+		}
+
+	}
+
+	function render () {
+
+		game.debug.inputInfo(32, 32);
+
+	}
+
+	return {
+		init: init
+	};
+
+};
+
+//console.log(game);
+//game.init;
