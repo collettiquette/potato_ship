0d863fa3dbafc9020f512a1a9243f5cd0659c82a
Kevin Collette <kevcollette@gmail.com>
2014-10-18 12:03:22 -0500

    Update stats controller to make use of Game methods.

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -7,17 +7,15 @@ var Stats = function(dispatcher,channel) {
 
   var update_score = function(game_id,new_score) {
 
-    dispatcher.trigger("update_score", { game_id: game_id, player_id: getCookie('player_id'), score: new_score });
+    dispatcher.trigger("update_score", { game_id: game_id, score: new_score });
   }
 
   var update_kills = function(game_id,new_kills) {
-    dispatcher.trigger("update_kills", { game_id: game_id, player_id:
-    getCookie('player_id'), kills: new_kills });
+    dispatcher.trigger("update_kills", { game_id: game_id, kills: new_kills });
   }
 
   var update_deaths = function(game_id,new_deaths) {
-    dispatcher.trigger("update_deaths", { game_id: game_id, player_id:
-    getCookie('player_id'), deaths: new_deaths })
+    dispatcher.trigger("update_deaths", { game_id: game_id, deaths: new_deaths })
   }
 
   return {
--- app/controllers/sockets/stats_controller.rb
+++ app/controllers/sockets/stats_controller.rb
@@ -18,17 +18,7 @@ module Sockets
     private
 
     def get_stat_for_player
-      games = JSON.parse(redis[:games])
-      game = find_game_by_id(games,message[:game_id])
-      return find_stat(game.stats,message[:player_id])
-    end
-
-    def find_game_by_id(games,id)
-      games.select {|g| Game.parse(g).id == id }
-    end
-
-    def find_stat(stats,player_id)
-      stats.select {|s| s.player_id == player_id}
+      return games[message[:game_id]][client_id]
     end
 
   end
