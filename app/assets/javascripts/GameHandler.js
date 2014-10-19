fd5a673a13d876008cbbb5a46046119d13f406dd
Brandon Cash <brandon@40digits.com>
2014-10-19 16:47:46 -0500

    #winning

--- app/assets/javascripts/GameHandler.js
+++ app/assets/javascripts/GameHandler.js
@@ -2,6 +2,9 @@ var GameHandler = function () {
   var init = function () {
     ConnectionHandler.channel.bind('end_game', function (data) {
       console.log('Game over');
+      setTimeout(function () {
+      	$('#winning').fadeIn(500);
+      }, 1000)
     });
 
   };
