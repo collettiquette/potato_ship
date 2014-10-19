97562644ae4ff784c2ef2fbe968e96a80d801080
JC Wilcox <84jwilcox@gmail.com>
2014-10-19 13:47:57 -0500

    Fix game store

--- app/controllers/sockets/application_controller.rb
+++ app/controllers/sockets/application_controller.rb
@@ -12,7 +12,7 @@ module Sockets
     end
 
     def games
-      controller_store[:games] ||= JSON.parse(redis[:games] ||= {}.to_json)
+      @games ||= JSON.parse(redis[:games] ||= {}.to_json)
         .each_with_object({}) do |(game_id, game_hash), result|
           result[game_id.to_i] = Game.parse(game_hash)
         end
@@ -20,7 +20,6 @@ module Sockets
 
     def store_games
       redis[:games] = games.to_json
-      controller_store.delete(:games)
     end
   end
 end
