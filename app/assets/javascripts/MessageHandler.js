bd190e55e25faa73527c1f7e579c141d19dad48e
Brandon Cash <brandon@40digits.com>
2014-10-19 18:00:05 -0500

    Nice looking messages

--- app/assets/javascripts/MessageHandler.js
+++ app/assets/javascripts/MessageHandler.js
@@ -2,11 +2,17 @@ var MessageHandler = function (dispatcher, channel) {
 
   var dispatcher,
       channel,
-      $message_container = $("#message-container");
+      $message_container = $("#message-container ul");
 
   var init = function () {
     channel.bind('new_message', function(data) {
-      $message_container.append("<li>" + data.message + "</li>");
+      $message_container.append("<li class='new'>" + data.message + "</li>");
+
+      setTimeout(function () {
+        $message_container.children('li.new').first().removeClass('new').fadeOut(1000, function() {
+          $(this).remove();
+        });
+      }, 5000);
     });
   };
 
